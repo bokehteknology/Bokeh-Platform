465712e4e933d406e63ee76faf3cb0a439d42c53
Carlo <carlos94@tiscali.it>
2011-06-22 16:45:31 +0200

    Added test.php plugin

---
+++ Bokeh-Platform/plugins/test/test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* @package Bokeh Platform
+* @version $Id$
+* @copyright (c) 2011 Bokeh Platform
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_BOKEH'))
+{
+	exit;
+}
+
+?>
