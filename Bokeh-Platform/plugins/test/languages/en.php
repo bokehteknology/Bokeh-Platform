c6cbd9d8218a2796c9f4629c4bf3e0d4d75027c4
Carlo <carlos94@tiscali.it>
2011-06-23 22:58:32 +0200

    Added testing plugin (test.php) as controller, languages (en, it) and template

---
+++ Bokeh-Platform/plugins/test/languages/en.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*
+* en [English]
+*
+* @plugin test
+* @package language
+* @copyright (c) 2011 Bokeh Platform
+* @author Carlo [uid: 2]
+* @version 1.0.0
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_BOKEH')) {
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// UTF-8 (no BOM)
+// Some characters
+// ’ » “ ” …
+
+$lang = array_merge($lang, array(
+	'PLUGIN_TEST_TITLE'	=> 'Test Plugin',
+	'PLUGIN_TEST_BODY'	=> 'This is a testing plugin!'
+));
+?>
--- Bokeh-Platform/plugins/test/test.php
+++ Bokeh-Platform/plugins/test/test.php
@@ -16,4 +16,28 @@ if (!defined('IN_BOKEH'))
 	exit;
 }
 
+class plugin_test
+{
+	var $is_controller = true;
+	var $load_lang = true;
+
+	function plugin_test()
+	{
+		global $root_path, $plugin_name;
+
+		$this->template_dir = $root_path . 'plugins/' . $plugin_name . '/templates/';
+
+		# here you can put some code for init your plugin
+	}
+
+	function index()
+	{
+		global $smarty;
+	
+		page_header('PLUGIN_TEST_TITLE');
+		$smarty->display($this->template_dir . 'home_body.html');
+		page_footer();
+	}
+}
+
 ?>
