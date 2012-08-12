5f8509af3c06894c4ff2f9144cad9f9e0faa8fc5
Carlo <carlos94@tiscali.it>
2012-08-12 17:34:25 +0200

    [PHPUnit] Import Travis and PHPUnit configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*
+* @package bp_tests
+* @copyright (c) 2012 Bokeh Teknology
+* @license http://opensource.org/licenses/gpl-3.0.html GNU GPL v3
+*
+*/
+define('IN_BOKEH', true);
+$root_path = './Bokeh-Platform/';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($root_path . 'includes/bootstrap.' . $phpEx);
