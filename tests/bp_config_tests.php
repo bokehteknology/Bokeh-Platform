8ad95ee0aba392aa6770d5176336cdedf84a0cd8
Carlo <carlos94@tiscali.it>
2012-08-12 19:37:54 +0200

    [PHPUnit] bp_conifg_tests.php

--- tests/bp_config_tests.php
+++ tests/bp_config_tests.php
@@ -9,10 +9,28 @@
 
 class bp_config_tests extends PHPUnit_Framework_TestCase
 {
-	public function test_sys_read()
+	public function test_convert_to_object()
+	{
+		$config = new config();
+
+		$config->read('database', false);
+
+		$this->assertTrue(isset($config->db->type));
+	}
+
+	public function test_sys_get()
 	{
 		$config = new config();
 
 		$this->assertEquals('www.mysite.com', $config->sys->server_name);
+	}
+
+	public function test_sys_update()
+	{
+		$config = new config();
+
+		$config->sys->server_name = 'www.yoursite.com';
+
+		$this->assertEquals('www.yoursite.com', $config->sys->server_name);
 	}
 }
