1763102b4b6a7fcdceeeaa295157104d03efedc2
Carlo <carlos94@tiscali.it>
2012-08-13 12:48:17 +0200

    [PHPUnit] Updated tests

--- tests/bp_database_tests.php
+++ tests/bp_database_tests.php
@@ -13,27 +13,22 @@ class bp_database_tests extends PHPUnit_Framework_TestCase
 
 	private function _start()
 	{
-		global $_ENV;
+		global $_SERVER, $root_path, $phpEx;
 
 		$config = new config();
-		$config->db->type = $_ENV['DB'];
+
+		$config->db->type = $_SERVER['BP_DB_TYPE'];		
+		$config->db->host = $_SERVER['BP_DB_HOST'];
+		$config->db->port = $_SERVER['BP_DB_PORT'];
+		$config->db->name = $_SERVER['BP_DB_NAME'];
+		$config->db->user = $_SERVER['BP_DB_USER'];
+		$config->db->pass = $_SERVER['BP_DB_PASS'];
+		$config->db->prefix = '_bokeh';
 
 		require($root_path . 'includes/database/' . $config->db->type . '.' . $phpEx);
 
 		$database_class_name = 'database_' . $config->db->type;
 		$this->db = new $database_class_name();
-
-		switch($config->db->type)
-		{
-			case 'mysql':
-				$config->db->host = 'localhost';
-				$config->db->port = 3306;
-				$config->db->name = 'bokeh_platform';
-				$config->db->user = 'root';
-				$config->db->pass = '';
-				$config->db->prefix = '_bokeh';
-				break;
-		}
 
 		return $this->db->sql_connect($config->db->host, $config->db->port, $config->db->user, $config->db->pass, $config->db->name);
 	}
