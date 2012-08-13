99634af0c196799bf08c979e0b0c1ff9e82769b7
Carlo <carlos94@tiscali.it>
2012-08-13 11:15:20 +0200

    [PHPUnit] Updated tests

---
+++ tests/bp_smarty_tests.php
@@ -0,0 +1,31 @@
+<?php
+/**
+*
+* @package bp_tests
+* @copyright (c) 2012 Bokeh Teknology
+* @license http://opensource.org/licenses/gpl-3.0.html GNU GPL v3
+*
+*/
+
+class bp_smarty_tests extends PHPUnit_Framework_TestCase
+{
+	# Load Smarty
+	public function test_load()
+	{
+		global $root_path;
+
+		$smarty = new Smarty();
+
+		$smarty->setTemplateDir($root_path . 'views/');
+		$smarty->setCompileDir($root_path . 'cache/');
+		$smarty->setCacheDir($root_path . 'cache/');
+		$smarty->force_compile = false;
+		$smarty->compile_check = true;
+
+		$this->asserEquals($root_path . 'views/', $smarty->getTemplateDir('0'));
+		$this->asserEquals($root_path . 'cache/', $smarty->getCompileDir());
+		$this->asserEquals($root_path . 'views/', $smarty->getCacheDir());
+		$this->assertFalse($smarty->force_compile);
+		$this->assertTrue($smarty->compile_check);
+	}
+}
