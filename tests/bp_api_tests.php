a05d4a549cbffea022d646d8c594472e7b7c686f
Carlo <carlos94@tiscali.it>
2012-08-16 18:26:24 +0200

    [CI] Updated tests

---
+++ tests/bp_api_tests.php
@@ -0,0 +1,31 @@
+<?php
+/**
+*
+* @package bp_tests
+* @copyright (c) 2012 Bokeh Teknology
+* @license http://opensource.org/licenses/gpl-3.0.html GNU GPL v3
+*
+*/
+
+class bp_api_tests extends PHPUnit_Framework_TestCase
+{
+	# Do a API request
+	public function test_do_request()
+	{
+		global $_SERVER, $bokeh_version;
+
+		$_SERVER['SERVER_NAME'] = 'pppX.worker.travis-ci.org';
+		$bokeh_version = '0.0.0';
+
+		$request = api_request('bokeh_platform', 'dev');
+		$request_status = (bool) $request
+
+		$this->assertTrue($request_status);
+
+		if ($request_status && (!isset($request['news_type']) || $request['news_type'] != 0))
+		{
+			$this->assertTrue(isset($request['version']));
+			$this->assertFalse(isset($request['error']));
+		}
+	}
+}
